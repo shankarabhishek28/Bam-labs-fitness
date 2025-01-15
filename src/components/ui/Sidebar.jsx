d62fbe6089644e05f924108330d5fb5ada6a0f3d
Abhishek-shankar <developer2@ideausher.com>
2025-01-15 19:18:49 +0530

    AS:design changes

--- src/components/ui/Sidebar.jsx
+++ src/components/ui/Sidebar.jsx
@@ -31,7 +31,7 @@ export default function Sidebar() {
       };
 
     return (
-        <div className="w-full bg-secondary fixed bottom-0 left-0 h-16 md:h-dvh md:sticky md:top-0 md:left-0 md:bg-secondary md:flex md:flex-wrap md:flex-col z-30">
+        <div className="w-full bg-black fixed bottom-0 left-0 h-16 md:h-dvh md:sticky md:top-0 md:left-0 md:bg-black md:flex md:flex-wrap md:flex-col z-30">
             <div className="p-6 pb-12 text-2xl mx-auto font-bold">
                 <Image src='/BamLogo.svg' width={100} height={100} />
             </div>
